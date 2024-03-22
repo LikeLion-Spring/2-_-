8e96e5010d427e3613769779f0e0eed83ec2db86
chabin37 <81852704+chabin37@users.noreply.github.com>
2024-03-22 15:18:12 +0900

    Delete src/main/java/likeLion1/practice/order/order directory

--- src/main/java/likeLion1/practice/order/order/DiscountPolicy.java
+++
@@ -1,7 +0,0 @@
-package likeLion.practice.order;
-
-import likeLion.practice.member.Member;
-
-public interface DiscountPolicy {
-    int discount(Member m, int price);
-}
--- src/main/java/likeLion1/practice/order/order/OrderService.java
+++
@@ -1,7 +0,0 @@
-package likeLion.practice.order;
-
-import likeLion.practice.member.Member;
-
-public interface OrderService {
-    Order createOrder(Long id, String productName, int price);
-}
